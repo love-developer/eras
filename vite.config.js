cd815f08452f6f4149cc2178c968a6e6dcd8e2d7
love-developer <lovekumaramrani13@gmail.com>
2026-01-31 03:19:07 +0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+    base: '/',   // 👈 YE ZAROORI HAI
   build: {
     rollupOptions: {
       external: ["sonner"],
